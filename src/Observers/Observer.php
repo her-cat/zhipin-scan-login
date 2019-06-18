3b0653051f03da98e30519f2f7a111cefa16f21e
her-cat <hxhsoft@foxmail.com>
2019-06-18 01:47:15 +0000

    Apply fixes from StyleCI

--- src/Observers/Observer.php
+++ src/Observers/Observer.php
@@ -1,7 +1,15 @@
 <?php
 
-namespace HerCat\ZhipinScanLogin\Observers;
+/*
+ * This file is part of the her-cat/zhipin-scan-login.
+ *
+ * (c) her-cat <hxhsoft@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
+namespace HerCat\ZhipinScanLogin\Observers;
 
 use HerCat\Exceptions\InvalidArgumentException;
 use HerCat\Exceptions\ObserverNotFoundException;
@@ -23,6 +31,7 @@ class Observer
 
     /**
      * Observer constructor.
+     *
      * @param Application $app
      */
     public function __construct(Application $app)
