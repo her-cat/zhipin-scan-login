ff556c828609b04d89339582fee98b14138c295f
her-cat <hxhsoft@foxmail.com>
2019-06-18 01:47:15 +0000

    Apply fixes from StyleCI

--- src/Application.php
+++ src/Application.php
@@ -1,7 +1,15 @@
 <?php
 
-namespace HerCat\ZhipinScanLogin;
+/*
+ * This file is part of the her-cat/zhipin-scan-login.
+ *
+ * (c) her-cat <hxhsoft@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
+namespace HerCat\ZhipinScanLogin;
 
 use Dotenv\Dotenv;
 use HerCat\ZhipinScanLogin\ServiceProviders\ServiceProviderInterface;
--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -1,7 +1,15 @@
 <?php
 
-namespace HerCat\ZhipinScanLogin;
+/*
+ * This file is part of the her-cat/zhipin-scan-login.
+ *
+ * (c) her-cat <hxhsoft@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
+namespace HerCat\ZhipinScanLogin;
 
 use Closure;
 use Exception;
@@ -29,6 +37,7 @@ class ExceptionHandler
 
     /**
      * ExceptionHandler constructor.
+     *
      * @param Application $app
      */
     public function __construct(Application $app)
@@ -72,7 +81,7 @@ class ExceptionHandler
      * @param $level
      * @param $message
      * @param string $file
-     * @param int $line
+     * @param int    $line
      *
      * @throws \ErrorException
      */
